a9c3f275763c4fde830491e67377132adf798fa2
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2021-05-04 21:28:35 -0300

    iniciada criação da estrutura do hgrafico de pizza

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,6 +3,7 @@ import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import WalletBox from '../../components/WalletBox';
 import MessageBox from '../../components/MessageBox';
+import PieChart from '../../components/PieChart';
 
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
@@ -179,6 +180,8 @@ const Dashboard: React.FC = () => {
                     footerText={message.footerText}
                     icon={message.icon}
                 />
+
+                <PieChart />
             </Content>
         </Container>
     );
